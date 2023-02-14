1e633ddaddcb43c70e8f8483aa2409e8a0dd96d1
localadmin <>
2023-02-15 03:22:37 +0900

    20230215: move - files

--- tkinter_console/utils/checkbuttonex.py
+++ tkinter_console/utils/checkbuttonex.py
@@ -6,8 +6,8 @@ __all__ = ['CheckButtonEx']
 
 class CheckButtonEx(ttk.Checkbutton):
     def __init__(self, parent, *args, **kwargs):
-        self.__flag: bool = kwargs.pop('flag')
-        self.__text: str = kwargs['text']
+        self.__flag: bool          = kwargs.pop('flag')
+        self.__text: str           = kwargs['text']
         self.__var : tk.BooleanVar = tk.BooleanVar()
 
         self.__var.set(self.__flag)
