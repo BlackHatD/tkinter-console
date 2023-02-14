c7d0fb10f756e7d9efef70db5152d2977a2dbaf4
localadmin <>
2023-02-14 22:33:21 +0900

    20230214: fix - newline

--- tkinter_console/log_console/__init__.py
+++ tkinter_console/log_console/__init__.py
@@ -3,9 +3,3 @@ from tkinter_console.log_console.frames.console import *
 from tkinter_console.log_console.frames.selector import *
 from tkinter_console.log_console.frames.console_ex import *
 
-
-
-
-
-
-
