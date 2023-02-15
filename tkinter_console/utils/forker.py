08e3e4783115074a82408c4d88892f7d45c7d3b5
localadmin <>
2023-02-15 17:54:39 +0900

    20230215: fix - method (__init__) and rename file

--- tkinter_console/utils/forker.py
+++ tkinter_console/utils/forker.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
+import copy
 import io
 import sys
 import traceback
 from typing import Optional, Callable
 
-__all__ = ['StdForkDecorator']
+__all__ = ['std_fork_decorator']
 
-class StdForkDecorator:
+class std_fork_decorator:
     stdin     = sys.__stdin__.name
     stdout    = sys.__stdout__.name
     stderr    = sys.__stderr__.name
@@ -25,13 +26,16 @@ class StdForkDecorator:
         # function result
         self._result = None
 
-        self._output = {self.stdin      : ''
-                        , self.stdout   : ''
-                        , self.stderr   : ''
-                        , self.traceback: ''}
+        # for callback args
+        self._org_output = {self.stdin      : ''
+                            , self.stdout   : ''
+                            , self.stderr   : ''
+                            , self.traceback: ''}
+        self._output    = copy.deepcopy(self._org_output)
 
 
     def __call__(self, func):
+        # set function at first
         self._func = func
 
         def wrapper(*args, **kwargs):
@@ -44,6 +48,7 @@ class StdForkDecorator:
                 self._result = self._func(*args, **kwargs)
 
             except Exception:
+                # set traceback
                 self._output[self.traceback] = traceback.format_exc()
 
             finally:
@@ -61,24 +66,30 @@ class StdForkDecorator:
                 except Exception:
                     traceback.print_exc()
 
+                # restore output
+                self._output = copy.deepcopy(self._org_output)
+
             return self._result
 
+        # return wrapper function's address
         return wrapper
+
 
     @staticmethod
     def __override_std():
+        """ override std """
         sys.stdin  = io.TextIOWrapper(io.BytesIO(), sys.stdin.encoding)
         sys.stdout = io.TextIOWrapper(io.BytesIO(), sys.stdout.encoding)
         sys.stderr = io.TextIOWrapper(io.BytesIO(), sys.stderr.encoding)
 
-
     def __restore_std(self):
+        """ restore std """
         sys.stdin  = self._org_stdin
         sys.stdout = self._org_stdout
         sys.stderr = self._org_stderr
 
-
     def __set_output(self, name, std):
+        """ set output for callback function """
         std.seek(0)
         self._output[name] = std.read()
         std.close()
