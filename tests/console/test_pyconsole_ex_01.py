b5c08266333fea45ec8a0d36e7e5fe9cbef52cb7
localadmin <>
2023-02-18 04:00:21 +0900

    20230218: create - testcase

---
+++ tests/console/test_pyconsole_ex_01.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import tkinter as tk
+from tkinter_console.console.pyconsole_ex import PyConsoleEx
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('PyConsoleEx Test01')
+
+    console = PyConsoleEx(root, locals())
+    console.init().pack()
+    console.focus()
+
+    root.mainloop()
+
+
