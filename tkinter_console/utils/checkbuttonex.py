d627ee55db7d7ebbb407d128db8486956dc82c8a
localadmin <>
2023-02-17 14:46:28 +0900

    20230217: remove unnecessary parameter

--- tkinter_console/utils/checkbuttonex.py
+++ tkinter_console/utils/checkbuttonex.py
@@ -7,7 +7,6 @@ __all__ = ['CheckButtonEx']
 class CheckButtonEx(ttk.Checkbutton):
     def __init__(self, parent, *args, **kwargs):
         self.__flag: bool          = kwargs.pop('flag')
-        self.__text: str           = kwargs['text']
         self.__var : tk.BooleanVar = tk.BooleanVar()
 
         self.__var.set(self.__flag)
